7a78de969d60519da0e37e51ed0c475dccdefda5
A. Blakeslee Moore <abmoore3@usfca.edu>
2022-03-19 00:28:49 -0700

    added automatic setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+$WRKDIR=$HOME/sh/clearstar
+
+if [ ! -d "$HOME/sh" ]
+then
+  mkdir $HOME/sh
+  mkdir $WRKDIR
+else
+  if [ ! -d "$WRKDIR" ]
+  then
+    mkdir $WRKDIR
+  else
+    mv ./* $WRKDIR/
+    chmod a+x $WRKDIR/*
+    echo -e "\nalias clearstar=$WRKDIR/star_connect.sh" >> .bashrc
+    echo -e "\nalias csc=clearstar" >> .bashrc
+    source .bashrc
+  fi
+fi 
